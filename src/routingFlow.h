e87da9e34939070f60852bd29cf70cfb22564dbe
fc3stw <fc3stw@gmail.com>
2020-06-21 03:47:27 +0800

    add interface for placement

--- src/routingFlow.h
+++ src/routingFlow.h
@@ -3,6 +3,11 @@
 class Router{
     Chip &_chip;
     vector<Net*> &_net_list;
+    vector<Net*> _sorted_net_list;
+    bool sort_net_by_hpwl(Net *n1, Net *n2);
 public:
     Router(Chip &chip, vector<Net*> &net_list);
+
+    void routing_flow();
+    set<Pos3d> collect_steiner_points(Net *net);
 };
